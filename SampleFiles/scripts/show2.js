d75a49f013c071943d4f7bbc1eed06b372353eb1
Gurmehar <thedovesscar@gmail.com>
2019-11-09 03:10:55 -0800

    Completed EC Part 2, ShowAllTweets(), fully replicating the original tweets style

--- SampleFiles/scripts/show2.js
+++ SampleFiles/scripts/show2.js
@@ -21,6 +21,39 @@ const tweet3 = function () {
     document.querySelector('#tweet3 .message').textContent = tweets.statuses[2].text;
 }
 
-function showAll() {
+const showAll = () => {
+
+    var tweetsLength = tweets.statuses.length;
+
+    for (var i = 3; i < tweetsLength; i++) {
+
+        var newTweet = document.createElement('article');
+        newTweet.className = "tweet";
+
+        var icon = document.createElement('i');
+        icon.className = "fa fa-user";
+        newTweet.append(icon);
+
+        var anc = document.createElement('a');
+        anc.textContent = " ";
+        newTweet.append(anc);
+
+        var user = document.createElement('span');
+        user.className = "username";
+        user.innerHTML = tweets.statuses[i].user.name + " @" + tweets.statuses[i].user.screen_name + " &bull; ";
+        newTweet.append(user);
+
+        var date = document.createElement('span');
+        date.className = "date";
+        date.textContent = tweets.statuses[i].created_at;
+        newTweet.append(date);
+
+        var message = document.createElement('div');
+        message.className = "message";
+        message.textContent = tweets.statuses[i].text;
+        newTweet.append(message);
+
+        document.getElementById('all').appendChild(newTweet)
+    }
 
 }
