41091a37690c916f5139606bbe4b11cd15d72c38
Gurmehar <thedovesscar@gmail.com>
2019-11-09 02:24:30 -0800

    Completed EC part1, show Tweet1-3()

--- SampleFiles/scripts/show2.js
+++ SampleFiles/scripts/show2.js
@@ -1,2 +1,26 @@
-console.log(tweets)
+//console.log(tweets)
 
+const tweet1 = () => {
+
+    document.querySelector('#tweet1 .username').textContent = tweets.statuses[0].user.name + " @" + tweets.statuses[0].user.screen_name;
+    document.querySelector('#tweet1 .date').textContent = tweets.statuses[0].created_at;
+    document.querySelector('#tweet1 .message').textContent = tweets.statuses[0].text;
+}
+
+const tweet2 = () => {
+
+    document.querySelector('#tweet2 .username').textContent = tweets.statuses[1].user.name + " @" + tweets.statuses[1].user.screen_name;
+    document.querySelector('#tweet2 .date').textContent = tweets.statuses[1].created_at;
+    document.querySelector('#tweet2 .message').textContent = tweets.statuses[1].text;
+}
+
+const tweet3 = function () {
+
+    document.querySelector('#tweet3 .username').textContent = tweets.statuses[2].user.name + " @" + tweets.statuses[2].user.screen_name;
+    document.querySelector('#tweet3 .date').textContent = tweets.statuses[2].created_at;
+    document.querySelector('#tweet3 .message').textContent = tweets.statuses[2].text;
+}
+
+function showAll() {
+
+}
